a5ee073ccd04f19ba5a3f233c4e6e717f43085cd
ryanmanderson <anderson.m.ryan@icloud.com>
2020-02-06 13:01:40 -0500

    Adds a scaling and pulsing effect to the SightingAnnotationView

--- Ellerbe Creek/Common/UI/SightingAnnotationView.swift
+++ Ellerbe Creek/Common/UI/SightingAnnotationView.swift
@@ -18,6 +18,27 @@ class SightingAnnotationView: MGLAnnotationView {
         layer.cornerRadius = bounds.width * 0.5
         layer.borderWidth = 4
         layer.borderColor = Colors.darkOrange.cgColor
+        
+        // Add pulsing effect to the root layer
+        let opacityAnimation = CABasicAnimation(keyPath: #keyPath(CALayer.opacity))
+        opacityAnimation.duration = 2
+        opacityAnimation.fromValue = 0.85
+        opacityAnimation.toValue = 1
+        opacityAnimation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeInEaseOut)
+        opacityAnimation.autoreverses = true
+        opacityAnimation.repeatCount = .greatestFiniteMagnitude
+        layer.add(opacityAnimation, forKey: "animateOpacity")
+        
+        // Add scaling effect to the root layer
+        let scaleAnimation = CABasicAnimation(keyPath: "transform.scale")
+        scaleAnimation.duration = 2
+        scaleAnimation.fromValue = Double.random(in: 0.8..<0.95)
+        scaleAnimation.toValue = Double.random(in: 1.05..<1.2)
+        scaleAnimation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeInEaseOut)
+        scaleAnimation.autoreverses = true
+        scaleAnimation.repeatCount = .greatestFiniteMagnitude
+        layer.add(scaleAnimation, forKey: "animateScale")
+        
     }
      
     override func setSelected(_ selected: Bool, animated: Bool) {
